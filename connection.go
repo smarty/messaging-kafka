ad53389bd234b530499f8a23de96778028c0fbaa
Jonathan Oliver <jonathan.s.oliver42@gmail.com>
2021-04-07 13:07:48 -0600

    Use a pointer.

--- connection.go
+++ connection.go
@@ -13,24 +13,24 @@ type defaultConnection struct {
 }
 
 func newConnection(config configuration, parent context.Context) messaging.Connection {
-	this := defaultConnection{config: config}
+	this := &defaultConnection{config: config} // pointer because defaultConnection needs to be comparable for connection pool
 	this.lifecycle, this.cancel = context.WithCancel(parent)
 	return this
 }
 
-func (this defaultConnection) Reader(_ context.Context) (messaging.Reader, error) {
+func (this *defaultConnection) Reader(_ context.Context) (messaging.Reader, error) {
 	return newReader(this.config, this.lifecycle), nil
 }
 
-func (this defaultConnection) Writer(_ context.Context) (messaging.Writer, error) {
+func (this *defaultConnection) Writer(_ context.Context) (messaging.Writer, error) {
 	return newWriter(this.config, this.lifecycle, false), nil
 }
 
-func (this defaultConnection) CommitWriter(_ context.Context) (messaging.CommitWriter, error) {
+func (this *defaultConnection) CommitWriter(_ context.Context) (messaging.CommitWriter, error) {
 	return newWriter(this.config, this.lifecycle, true), nil
 }
 
-func (this defaultConnection) Close() error {
+func (this *defaultConnection) Close() error {
 	this.cancel()
 	return nil
 }
